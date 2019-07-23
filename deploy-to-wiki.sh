b955c5bd127d624a538de5bb0e054c568f6a25dd
Blake Mills <welzie@gmail.com>
2019-07-23 11:59:55 -0500

    progress on shell

--- deploy-to-wiki.sh
+++ deploy-to-wiki.sh
@@ -1,3 +1,7 @@
 # Shell script that will deploy the master branch to the github
 # repo behind the intro wiki https://github.com/LaunchCodeEducation/intro-to-professional-web-dev/wiki
 
+# WARNING: This script assumes you have added a remote named upstream that points to git@github.com:LaunchCodeEducation/intro-to-professional-web-dev.wiki.git
+# If you haven't, please run this command: git remote add upstream git@github.com:LaunchCodeEducation/intro-to-professional-web-dev.wiki.git
+
+git pull upstream master
