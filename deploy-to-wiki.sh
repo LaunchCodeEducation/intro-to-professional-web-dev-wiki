3a6d49a21564918d5fb3a8c6b7820e788398fe02
Blake Mills <welzie@gmail.com>
2019-07-23 12:21:31 -0500

    more work on sh

--- deploy-to-wiki.sh
+++ deploy-to-wiki.sh
@@ -4,4 +4,8 @@
 # WARNING: This script assumes you have added a remote named upstream that points to git@github.com:LaunchCodeEducation/intro-to-professional-web-dev.wiki.git
 # If you haven't, please run this command: git remote add upstream git@github.com:LaunchCodeEducation/intro-to-professional-web-dev.wiki.git
 
+# WARNING: It's possible that pulling in master from origin or upstream could cause merge conflicts. You will need to resove those.
+
+git pull origin master
 git pull upstream master
+git push upstream master
