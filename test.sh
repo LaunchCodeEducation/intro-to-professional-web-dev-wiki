590ba55f5109a085a90ab2240622c5e74c789a3f
Blake Mills <welzie@gmail.com>
2019-07-23 15:26:44 -0500

    removed test file

--- test.sh
+++
@@ -1,7 +0,0 @@
-upstream="git@github.com:LaunchCodeEducation/intro-to-professional-web-dev.wiki.git"
-
-if [ $(git config --get remote.upstream.url) != "$upstream" ]; then
-    echo "Strings are not equal"
-else
-    echo "Strings are equal"
-fi
