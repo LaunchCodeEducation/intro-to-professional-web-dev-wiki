9628594ac34757e681022795c7d1c4bf8d0441cc
Blake Mills <welzie@gmail.com>
2019-07-29 16:07:41 -0500

    updated links and images

--- _Footer.md
+++ _Footer.md
@@ -1,3 +1,3 @@
 ### Want to contribute to this Wiki?
 
-[Fork it and send a pull request.](https://github.com/LaunchCodeEducation/intro-to-professional-web-dev-wiki)
+[Fork it and send a pull request.](https://github.com/LaunchCodeEducation/intro-to-professional-web-dev-wiki-wiki)
--- deploy-to-wiki.sh
+++ deploy-to-wiki.sh
@@ -1,5 +1,5 @@
 # Shell script that will deploy the master branch to the github
-# repo behind the intro wiki https://github.com/LaunchCodeEducation/intro-to-professional-web-dev/wiki
+# repo behind the intro wiki https://github.com/LaunchCodeEducation/intro-to-professional-web-dev-wiki/wiki
 
 # WARNING: This script assumes you have added a remote named upstream that points to git@github.com:LaunchCodeEducation/intro-to-professional-web-dev.wiki.git
 # If you haven't, please run this command: git remote add upstream git@github.com:LaunchCodeEducation/intro-to-professional-web-dev.wiki.git
